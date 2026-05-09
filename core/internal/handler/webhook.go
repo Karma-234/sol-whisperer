b7a7fc09fc77f7ee81a83ee5a69ce9c11bf23ce3
Karma-234 <busari1kamal@gmail.com>
2026-05-09 20:59:24 +0100

    add: minimal webhook security

--- core/internal/handler/webhook.go
+++ core/internal/handler/webhook.go
@@ -1,9 +1,22 @@
 package handler
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/karma-234/sol-whisperer/core/internal/types"
 )
 
 func WebHookHandler(c *fiber.Ctx) error {
+	if auth := c.Get("Authorization"); auth != "Bearer "+os.Getenv("WEBHOOK_SECRET") {
+		return c.Status(401).SendString("Unauthorized")
+	}
+	var payload types.HeliusEnhancedWebhookPayload
+	if err := c.BodyParser(&payload); err != nil {
+		return c.Status(400).SendString("Invalid payload")
+	}
+	go func() {
+
+	}()
 	return c.SendStatus(200)
 }
