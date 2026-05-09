015e615d159c7f42be05d779952e2f2f2604ded3
Karma-234 <busari1kamal@gmail.com>
2026-05-09 20:20:43 +0100

    add: program names and dependencies

---
+++ core/helpers.go
@@ -0,0 +1,11 @@
+package core
+
+var ProgramNames = map[string]string{
+	"675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8": "raydium_v4",
+	"CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK": "raydium_clmm",
+	"CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C": "raydium_cpmm",
+	"6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P":  "pump_fun",
+	"pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA":  "pumpswap",
+	"BSwp6bEBihVLdqJRKGgzjcGLHkcTuzmSo1TQkHepzH8p": "bonkswap",
+	"FfYek5vEz23cMkWsdJwG2oa6EphsvXSHrGpdALN4g6W1": "letsbonk",
+}
